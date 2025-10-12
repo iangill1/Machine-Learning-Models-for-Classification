c7473088088d3a7b4d294f1a8a5c66808f634e29
iangill1 <i.gill3@universityofgalway.ie>
2025-10-12 20:56:49 +0100

    implemented SVM model with default parameters

--- main.py
+++ main.py
@@ -9,11 +9,36 @@ independent_cols = ["year", "temp", "humidity", "rainfall", "drought_code", "bui
 dependent_cols = "fire"
 
 #loading in training set with pandas library
-ds_training = pd.read_csv(training_file)
+wf_training = pd.read_csv(training_file)
+wf_test = pd.read_csv(test_file)
 #print(ds_training)
-print(ds_training.shape)
+#print(wf_training.shape)
+
+#setting up a matrix for independent variables from training set
+xmatrix_training = wf_training.loc[:, independent_cols]
+xmatrix_test = wf_test.loc[:, independent_cols]
+#print(xmatrix_training.head())
+
+#setting up vector for dependent var from training set
+ymatrix_training = wf_training.loc[:, dependent_cols]
+ymatrix_test = wf_test.loc[:, dependent_cols]
+#print(ymatrix_training.head())
 
 #import SVM package
+from sklearn import metrics
 from sklearn.svm import SVC
-svc_c = SVC(C=1.0)
+#create a model and use default values for hyperparameters first
+svm = SVC(C=1.0, gamma='scale')
+svm.fit(xmatrix_training, ymatrix_training)
 
+#calculate prediction for training set and test set
+training_prediction = svm.predict(xmatrix_training)
+test_prediction = svm.predict(xmatrix_test)
+
+#calculate accuracy of prediction
+training_accuracy = metrics.accuracy_score(ymatrix_training, training_prediction)
+test_accuracy = metrics.accuracy_score(ymatrix_test, test_prediction)
+
+#print result
+print("\ntraining set accuracy with default hyperparameters: ", training_accuracy)
+print("test set accuracy with default hyperparameters: ", test_accuracy)
